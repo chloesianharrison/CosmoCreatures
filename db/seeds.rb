975daf0caa76cc1582578af445ea15fe3d56d143
Chloe Harrison <chloesian.harrison@live.co.uk>
2024-05-08 15:10:51 +0100

    faker lowercase f

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require 'Faker'
+require 'faker'
 require "open-uri"
 Playtime.destroy_all
 puts "playtime destroyed"
